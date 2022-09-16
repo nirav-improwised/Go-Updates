2bc375bdc5f2b9d984139ba8ef14852eaae52681
nirav <nirav.gaglani@improwised.com>
2022-09-16 18:49:16 +0530

    fibo_find_num using recursive added

---
+++ XO/xo.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("----------------------Welcome to X O game----------------------------")
+	var player1i string
+	var player1j string
+	var player2i string
+	var player2j string
+	var playground [3][3]string
+	fmt.Println("Player 1-X: Enter single space separated row and column. Ex: 1 1")
+	fmt.Scan(&player1i, &player1j)
+	fmt.Println("Player 2-O: Enter single space separated row and column. Ex: 1 1")
+	fmt.Scan(&player2i, &player2j)
+
+}
